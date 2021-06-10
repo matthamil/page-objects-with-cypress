3a3bb5e2ed47934d6519147e444f4be7d51c6e52
Matt Hamil <matthamil@users.noreply.github.com>
2021-06-10 13:20:27 -0500

    tests: initial test suite without page objects

--- cypress/integration/photos.spec.js
+++ cypress/integration/photos.spec.js
@@ -1,16 +1,31 @@
+beforeEach(() => {
+  cy.visit('/')
+})
+
 it('should display 9 photos', () => {
 	cy.get('.photo').should('have.length', 9)
 })
 
 it('has 5 Likes on the first photo', () => {
   cy.get('.photo').first().within(() => {
-    expect(cy.get('.likes')).to.contain(5)
+    cy.get('.likes').should('have.text', '5')
   })
 })
 
 it('should allow the user to delete a photo', () => {
-  cy.get('.photo').first().as('myPhoto').within(() => {
+  const TEST_ID = '1'
+  cy.get(`[data-cy="${TEST_ID}"`).within(() => {
     cy.get('.delete-btn').click()
   })
-  cy.get('@myPhoto').should('not.exist')
+  cy.get(`[data-cy="${TEST_ID}]"`).should('not.exist')
+})
+
+it('should display a message when no photos exist in the album', () => {
+  cy.get('.photo').each(($photo) => {
+    cy.wrap($photo).within(() => {
+      cy.get('.delete-btn').click()
+    })
+  })
+  const MESSAGE = "You don't have any photos in this album."
+  cy.contains(MESSAGE)
 })
