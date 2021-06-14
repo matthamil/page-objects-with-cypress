61dc7a02b0df443c67da2acfef7fbb22f8374e18
Matt Hamil <matthamil@users.noreply.github.com>
2021-06-14 10:16:50 -0500

    feat: refactor code to use template and view to allow programmatic access to app on window

---
+++ cypress/integration/photos-with-page-object.spec.js
@@ -0,0 +1,36 @@
+import {
+  getPhotos,
+  getPhotoByTestId,
+  getLikes,
+  deletePhoto
+} from '../support/photos.po'
+
+beforeEach(() => {
+  cy.visit('/')
+})
+
+it('should display 9 photos', () => {
+  getPhotos().should('have.length', 9)
+})
+      
+it('has 5 Likes on the first photo', () => {
+  const TEST_ID = 1
+  getLikes(TEST_ID).should('have.text', '5')
+})
+      
+it('should allow the user to delete a photo', () => {
+  const TEST_ID = '1'
+  deletePhoto(TEST_ID)
+  getPhotoByTestId(TEST_ID).should('not.exist')
+})
+      
+it('should display a message when no photos exist in the album', () => {
+	// remove all photos
+	cy.get('.photo').each(($photo) => {
+	  cy.wrap($photo).within(() => {
+	    cy.get('.delete-btn').click()
+	  })
+	})
+	const MESSAGE = "You don't have any photos in this album."
+	cy.contains(MESSAGE)
+})
---
+++ cypress/support/photos.po.js
@@ -0,0 +1,13 @@
+export const getPhotos = () => cy.get('.photo')
+
+export const getPhotoByTestId = (id) =>
+  cy.get(`[data-cy="${id}"]`)
+
+export const getLikes = (index) => 
+  getPhotoByTestId(index)
+    .find('.likes')
+
+export const deletePhoto = (index) => 
+  getPhotoByTestId(index).within(() => {
+    cy.get('.delete-btn').click()
+  })
